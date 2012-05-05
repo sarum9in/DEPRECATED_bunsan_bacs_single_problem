f5875d266dfa8230ead8d77073af17efac43a7f8
Filippov Aleksey <sarum9in@gmail.com>
2012-05-05 23:14:10 +0400

    bacs.problem.Driver interface changes.

--- src/bacs/problem/driver.py
+++ src/bacs/problem/driver.py
@@ -1,3 +1,44 @@
+class Tests(object):
+
+	def test_set(self):
+		"""
+			Returns set of test ids.
+		"""
+		raise NotImplementedError()
+
+	def data_set(self):
+		"""
+			Returns dictionary mapping data id
+			to file format (TEXT or BINARY).
+		"""
+		raise NotImplementedError()
+
+	def test(self, test_id, data_id):
+		"""
+			Returns path to the test data file.
+		"""
+		raise NotImplementedError()
+
+
+class Statements(object):
+	pass
+
+
+class Utilities(object):
+
+	def checker(self):
+		"""
+			Returns path to the checker folder.
+		"""
+		raise NotImplementedError()
+
+	def validator(self):
+		"""
+			Returns path to the validator folder.
+			Returns None if no validator is provided.
+		"""
+		raise NotImplementedError()
+
 
 class Driver(object):
 	"""
@@ -6,9 +47,43 @@ class Driver(object):
 		to problem internals.
 	"""
 
-	def problem(self):
+	def convert(self, destination):
 		"""
-			Returns bacs.xsd.problem.ProblemType.
+			Converts problem into internal format.
 		"""
 		raise NotImplementedError()
 
+	def problem(self):
+		"""
+			Returns bacs.xsd.problem.ProblemType instance.
+		"""
+		raise NotImplementedError()
+
+	def info(self):
+		"""
+			Returns bacs.xsd.problem.InfoType instance.
+		"""
+		raise NotImplementedError()
+
+	def tests(self):
+		"""
+			Returns Tests instance.
+		"""
+		raise NotImplementedError()
+
+	def statements(self):
+		"""
+			Returns Statements instance.
+		"""
+
+	def profiles(self):
+		"""
+			Returns bacs.xsd.problem.ProfilesType instance.
+		"""
+		raise NotImplementedError()
+
+	def utilities(self):
+		"""
+			Returns Utilities instance.
+		"""
+		raise NotImplementedError()

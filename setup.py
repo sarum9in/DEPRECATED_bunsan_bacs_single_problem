a0decb7f07af3d1fdb25d7d8a74fd79aa3e7d23f
Filippov Aleksey <sarum9in@gmail.com>
2012-05-06 02:07:28 +0400

    setup.py implementation.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+
+from distutils.core import setup
+
+setup(
+	name = 'BACS PROBLEM',
+	version = '0.0.1',
+	description = '',
+	author = 'Filippov Aleksey',
+	author_email = 'sarum9in@gmail.com',
+	license = "GPL",
+	url = 'http://bacs.cs.istu.ru',
+	packages = ['bacs.problem'],
+	package_dir = {'': 'src'}
+)
+

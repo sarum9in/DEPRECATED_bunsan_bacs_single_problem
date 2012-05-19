9025e733cb5f5291cca38f98e696696abe3fe353
Filippov Aleksey <sarum9in@gmail.com>
2012-05-20 01:46:33 +0400

    New class: bacs.problem.Loader.

--- src/bacs/problem/loader.py
+++ src/bacs/problem/loader.py
@@ -1,24 +1,33 @@
 from os.path import join
 
 
-def get_format(path):
-    """
-        Retrieves format name.
-    """
-    with open(join(path, "format")) as fmt:
-        return fmt.read().strip()
+class Loader(object):
+
+    def __init__(self, config):
+        """
+            config is dictionary that contains
+            the following options:
+            TODO
+        """
+        self._config = config
+
+    def get_format(self, path):
+        """
+            Retrieves format name.
+        """
+        with open(join(path, "format")) as fmt:
+            return fmt.read().strip()
+
+    def load(self, path):
+        """
+            Loads problem,
+            returns bacs.problem.Driver instance
+            implementing problem format.
+        """
+        format = self.get_format(path)
+        bacs = __import__('bacs.problem.drivers.{}'.format(format))
+        driver_module = getattr(bacs.problem.drivers, format)
+        return driver_module.Driver(path)
 
 
-def load(path):
-    """
-        Loads problem,
-        returns bacs.problem.Driver instance
-        implementing problem format.
-    """
-    format = get_format(path)
-    bacs = __import__('bacs.problem.drivers.{}'.format(format))
-    driver_module = getattr(bacs.problem.drivers, format)
-    return driver_module.Driver(path)
-
-
-__all__ = ['get_format', 'load']
+__all__ = ['Loader']
